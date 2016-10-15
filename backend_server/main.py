678ea54b3144f3390d3e2e5872715bab6ba9a73d
Eric Humphrey <ejhumphrey@spotify.com>
2016-10-15 17:07:22 -0400

    Fixed upload and GET for audio, now uses proper response formats.

--- backend_server/main.py
+++ backend_server/main.py
@@ -4,7 +4,14 @@ Please see README.md for instructions.
 
 Starting Locally
 ----------------
-$ dev_appserver.py .
+You have two options:
+
+  $ python main.py --local --debug
+
+Or, to use GCP backend by default:
+
+  $ dev_appserver.py .
+
 
 Endpoints
 ---------
@@ -16,13 +23,16 @@ Endpoints
 
 import argparse
 import datetime
-from flask import Flask, request
+from flask import Flask, request, Response
+from flask import send_file
+import io
 import json
 import logging
 import requests
 import os
 
 import pybackend.database
+import pybackend.mime
 import pybackend.storage
 import pybackend.utils
 
@@ -34,6 +44,8 @@ app = Flask(__name__)
 # TODO: This should be controlled by `app.yaml`, right?
 CLOUD_CONFIG = os.path.join(os.path.dirname(__file__), 'gcloud_config.json')
 app.config['cloud'] = json.load(open(CLOUD_CONFIG))
+
+SOURCE = "https://cosmir.github.io/open-mic/"
 
 
 @app.route('/')
@@ -48,9 +60,13 @@ def audio_upload():
 
     $ curl -F "audio=@some_file.mp3" localhost:8080/audio/upload
 
-    TODO: Define / adopt common response schema for endpoints.
+    TODOs:
+      - Store user data (who uploaded this? IP address?)
+      -
     """
-    response = dict(message='nothing happened', status=400)
+
+    data = json.dumps(dict(message='Invalid request'))
+    status = 400
     if request.method == 'POST':
         audio_data = request.files['audio']
         bytestring = audio_data.stream.read()
@@ -60,9 +76,9 @@ def audio_upload():
             project_id=app.config['cloud']['project_id'],
             **app.config['cloud']['storage'])
 
-        key = str(pybackend.utils.uuid(bytestring))
+        uri = str(pybackend.utils.uuid(bytestring))
         fext = os.path.splitext(audio_data.filename)[-1]
-        filepath = "{}{}".format(key, fext)
+        filepath = "{}{}".format(uri, fext)
         store.upload(bytestring, filepath)
 
         # Index in datastore
@@ -72,47 +88,56 @@ def audio_upload():
             **app.config['cloud']['database'])
         record = dict(filepath=filepath,
                       created=str(datetime.datetime.now()))
-        dbase.put(key, record)
-        record.update(key=key)
-        response.update(
-            status=200, data=record,
+        dbase.put(uri, record)
+        record.update(
+            uri=uri,
             message="Received {} bytes of data.".format(len(bytestring)))
+        status = 200
+        data = json.dumps(record)
 
-    return json.dumps(response)
+    resp = Response(data, status=status,
+                    mimetype=pybackend.mime.MIMETYPES['json'])
+    resp.headers['Link'] = SOURCE
+    return resp
 
 
-@app.route('/audio/<key>', methods=['GET'])
-def audio_download(key):
+@app.route('/audio/<uri>', methods=['GET'])
+def audio_download(uri):
     """
     To GET responses from this endpoint:
 
     $ curl -XGET localhost:8080/audio/bbdde322-c604-4753-b828-9fe8addf17b9
-
-    TODO: Define / adopt common response schema for endpoints.
     """
-    response = dict(message='Resource not found', status=404)
+
+    # Create the null response.
+    data = json.dumps(dict(message='Resource not found for `{}`'.format(uri)))
+    resp = Response(data, status=404,
+                    mimetype=pybackend.mime.MIMETYPES['json'])
+
     if request.method == 'GET':
         dbase = pybackend.database.Database(
             project_id=app.config['cloud']['project_id'],
             **app.config['cloud']['database'])
 
-        entity = dbase.get(key)
+        entity = dbase.get(uri)
         if entity is None:
-            response.update(status=404,
-                            message="Resource not found: {}".format(key))
-        else:
+            app.logger.info("Resource not found: {}".format(uri))
 
+        else:
             store = pybackend.storage.Storage(
                 project_id=app.config['cloud']['project_id'],
                 **app.config['cloud']['storage'])
-            fdata = store.download(entity['filepath'])
-            print(fdata[:500])
-            response.update(
-                status=200,
-                message="Success",
-                data=dict(bytes=str(fdata)))
-            return fdata
-    # return json.dumps(response)
+
+            data = store.download(entity['filepath'])
+            app.logger.debug("Returning {} bytes".format(len(data)))
+
+            resp = send_file(
+                io.BytesIO(data),
+                attachment_filename=entity['filepath'],
+                mimetype=pybackend.mime.mimetype_for_file(entity['filepath']))
+
+    resp.headers['Link'] = SOURCE
+    return resp
 
 
 @app.route('/annotation/submit', methods=['POST'])
@@ -166,6 +191,9 @@ def server_error(e):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument(
+        "--port", type=int, default=8080,
+        help="Port on which to serve.")
+    parser.add_argument(
         "--local",
         action='store_true', help="Use local backend services.")
     parser.add_argument(
@@ -179,4 +207,4 @@ if __name__ == '__main__':
         config = os.path.join(os.path.dirname(__file__), 'local_config.json')
         app.config['cloud'] = json.load(open(config))
 
-    app.run(debug=args.debug)
+    app.run(debug=args.debug, port=args.port)

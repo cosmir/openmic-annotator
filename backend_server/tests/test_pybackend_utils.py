5e9d54459a414fbd5533724fe469b952da3e048e
Eric Humphrey <ejhumphrey@spotify.com>
2016-12-03 17:29:55 -0500

    Potentially fixed AppEngine string conversion fail on uuid generation.

--- backend_server/tests/test_pybackend_utils.py
+++ backend_server/tests/test_pybackend_utils.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import pytest
 
 import pybackend.utils
@@ -5,6 +6,9 @@ import pybackend.utils
 
 def test_utils_uuid():
     data = 'blahblah'
+    # Expected hash determined by inspection
+    exp = "42d388f8-b1db-497f-aaf7-dab487f11290"
     uuid1 = pybackend.utils.uuid(data)
     assert uuid1 == pybackend.utils.uuid(data)
+    assert str(uuid1) == exp
     assert uuid1 != pybackend.utils.uuid('foobar')

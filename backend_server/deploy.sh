a6dced8d06c855cb1d32a04c42e669f259f26941
Eric Humphrey <ejhumphrey@spotify.com>
2016-12-29 12:22:09 -0500

    Final tweaks pre-PR

--- backend_server/deploy.sh
+++ backend_server/deploy.sh
@@ -6,6 +6,7 @@ if [ -z "$1" ]; then
     exit 1;
 fi
 
+# Update the gcloud config template and point to it as the default.
 sed "s/<YOUR_PROJECT_ID>/$1/" gcloud_config.json > .config.json
 
 # Run the test suite, tank deployment on failure.
